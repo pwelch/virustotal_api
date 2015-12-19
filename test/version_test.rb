74bbf08129ea176c1eb0f82a0bf8678a99add7c4
pwelch <paul@pwelch.net>
2015-12-19 16:03:29 -0500

    Update Gem Version for v0.2.0

--- test/version_test.rb
+++ test/version_test.rb
@@ -4,6 +4,6 @@ require './test/test_helper'
 class VirustotalAPIVTReportTest < Minitest::Test
   def test_version
     assert VirustotalAPI::VERSION.is_a?(String)
-    assert VirustotalAPI::VERSION, '0.1.0'
+    assert VirustotalAPI::VERSION, '0.2.0'
   end
 end
